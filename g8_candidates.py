c6b97fb5eeb00eb191d47709a1d5aabdd2dfa4a7
Richard Jenkins <rwj@serenity.uk.net>
2018-06-09 14:19:39 +0100

    fix g8

--- g8_candidates.py
+++ g8_candidates.py
@@ -20,8 +20,8 @@ g8 = []
 
 with conn.cursor() as cur:
 	now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-	print("{} - Running empty page query on wiki {}".format(now, wiki))
-	cur.execute("SELECT page_namespace, page_id, page_title, page_is_redirect, page_len FROM page talkpage WHERE talkpage.page_title NOT LIKE '%/%' AND talkpage.page_namespace IN (1,5,7,11,15,101,119) AND NOT EXISTS ( SELECT 1 FROM page mainpage WHERE mainpage.page_namespace=talkpage.page_namespace-1 AND mainpage.page_title=talkpage.page_title ) AND NOT EXISTS ( SELECT 1 FROM templatelinks WHERE talkpage.page_id=tl_from AND tl_title='G8-exempt' ) LIMIT 1000".format(limit))
+	print("{} - Running orphaned talk/CSD G8 candidate query on wiki {}".format(now, wiki))
+	cur.execute("SELECT page_namespace, page_title FROM page talkpage WHERE talkpage.page_title NOT LIKE '%/%' AND talkpage.page_namespace IN (1,5,7,11,15,101,119) AND NOT EXISTS ( SELECT 1 FROM page mainpage WHERE mainpage.page_namespace=talkpage.page_namespace-1 AND mainpage.page_title=talkpage.page_title ) AND NOT EXISTS ( SELECT 1 FROM templatelinks WHERE talkpage.page_id=tl_from AND tl_title='G8-exempt' ) LIMIT 1000".format(limit))
 	g8 = [(ns, e.decode("utf-8")) for ns, e in list(cur.fetchall())]
 
 pickle.dump(g8, open("/data/project/fireflytools/www/python/src/data/g8_candidates_{}.dat".format(wiki), "wb"))
