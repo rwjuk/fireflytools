9305e575a1258457b4b356e626151a9614e97a66
Richard Jenkins <rwj@serenity.uk.net>
2018-06-14 22:36:26 +0000

    update U2 script

--- u2_candidates.py
+++ u2_candidates.py
@@ -21,7 +21,7 @@ g8 = []
 with conn.cursor() as cur:
 	now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 	print("{} - Running CSD U2 candidate query on wiki {}".format(now, wiki))
-	cur.execute('select page_title from page left outer join user on (user.user_name = replace(page.page_title,"_"," ")) where user.user_name is null and page_namespace=2 and page_title not like "%/%" and page_title not like "%.%.%.%" and page_title not like "%:%:%" and page_is_redirect=False; limit {}'.format(limit))
+	cur.execute('select page_namespace, page_title from page left outer join user on (user.user_name = replace(page.page_title,"_"," ")) left outer join categorylinks on (page.page_id = cl_from) where cl_from is null and user.user_name is null and page_namespace=2 and page_title not like "%/%" and page_title not like "%.%.%.%" and page_title not like "%:%:%" and page_is_redirect=False limit {};'.format(limit))
 	g8 = [(ns, e.decode("utf-8")) for ns, e in list(cur.fetchall())]
 
 pickle.dump(g8, open("/data/project/fireflytools/www/python/src/data/u2_candidates_{}.dat".format(wiki), "wb"))
